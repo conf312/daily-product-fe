f395e6058c17badf41a7a62684f1a22c9144e1b5
hk0920 <gmlrb920@naver.com>
2023-01-15 16:50:41 +0900

    [modify] project init setting

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,14 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: {
+      textDefault: string;
+    };
+    device: {
+      mobile: string;
+      tablet: string;
+      desktop_s: string;
+    };
+  }
+}
